cfd2d1487add2714a2f1145bba3add2ee4353b8e
Dominic <mrdjfraser@gmail.com>
2017-03-17 11:10:00 +0000

    song class set up

---
+++ song.rb
@@ -0,0 +1,10 @@
+class Song
+
+  attr_reader :song_name, :song_length
+
+  def initialize(song_name, song_length)
+    @song_name = @song_name
+    @song_length = @song_length
+  end
+
+end
---
+++ specs/song_spec.rb
@@ -0,0 +1,15 @@
+require ("minitest/autorun")
+require ("minitest/rg")
+require_relative ("../song")
+
+class TestSong < Minitest::Test
+
+  def setup
+    @song1 = Song.new("song1","2.5")
+    @song2 = Song.new("song2","2")
+    @song3 = Song.new("song3","3.5")
+    @song4 = Song.new("song4","3.5")
+    @song5 = Song.new("song5","3")
+  end
+
+end
