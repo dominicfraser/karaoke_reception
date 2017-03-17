2d6e363a33cb824ae111927f37196db73f33c645
Dominic <mrdjfraser@gmail.com>
2017-03-17 11:40:39 +0000

    test_song_has_name passing

--- song.rb
+++ song.rb
@@ -3,8 +3,9 @@ class Song
   attr_reader :song_name, :song_length
 
   def initialize(song_name, song_length)
-    @song_name = @song_name
-    @song_length = @song_length
+    @song_name = song_name
+    @song_length = song_length
   end
 
 end
+
--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -5,11 +5,15 @@ require_relative ("../song")
 class TestSong < Minitest::Test
 
   def setup
-    @song1 = Song.new("song1","2.5")
-    @song2 = Song.new("song2","2")
-    @song3 = Song.new("song3","3.5")
-    @song4 = Song.new("song4","3.5")
-    @song5 = Song.new("song5","3")
+    @song1 = Song.new("song1",2.5)
+    @song2 = Song.new("song2",2)
+    @song3 = Song.new("song3",3.5)
+    @song4 = Song.new("song4",3.5)
+    @song5 = Song.new("song5",3)
+  end
+
+  def test_song_has_name
+    assert_equal("song1", @song1.song_name)
   end
 
 end
