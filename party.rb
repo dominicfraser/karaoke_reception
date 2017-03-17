fcf36cbcab1dabad164ded74478985e5eed5b068
Dominic <mrdjfraser@gmail.com>
2017-03-17 12:58:57 +0000

    test_update_lowest_wallet passing

--- party.rb
+++ party.rb
@@ -1,3 +1,5 @@
+#require "pry"
+
 class Party 
 
   attr_reader :guests, :lowest_wallet
@@ -5,10 +7,27 @@ class Party
   def initialize()
     @guests = []
     @lowest_wallet = 0
+    #@lowest_wallet.update_lowest_wallet
   end
 
   def add_guest(guest)
     @guests << guest
   end
 
+
+  def update_lowest_wallet()
+    total_wallet = []
+    @guests.each { |guest| 
+      total_wallet += [guest.wallet] }
+    @lowest_wallet = total_wallet.min
+  end
+
+
+
 end
+
+
+
+
+
+
--- specs/party_spec.rb
+++ specs/party_spec.rb
@@ -10,7 +10,7 @@ class TestParty < Minitest::Test
 
   def setup
     @guest1 = Guest.new("Jake",20,"song1")
-    @guest2 = Guest.new("Sam",20,"song2")
+    @guest2 = Guest.new("Sam",19,"song2")
     @song1 = Song.new("song1",2.5)
     @song2 = Song.new("song2",2)
     @party1 = Party.new()
@@ -20,5 +20,12 @@ class TestParty < Minitest::Test
     assert_equal([@guest1], @party1.add_guest(@guest1))
   end
 
+  def test_update_lowest_wallet
+    @party1.add_guest(@guest1)
+    @party1.add_guest(@guest2)
+    @party1.update_lowest_wallet
+
+    assert_equal(19, @party1.lowest_wallet)
+  end
 
 end
