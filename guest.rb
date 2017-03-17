fddcad01f6482da8497739e9662e09dfb8ade61d
Dominic <mrdjfraser@gmail.com>
2017-03-17 12:18:50 +0000

    test_guest_has_name passing

--- guest.rb
+++ guest.rb
@@ -1,9 +1,11 @@
 class Guest
 
-  attr_reader
+  attr_reader :g_name, :wallet, :fav_song
 
-  def initialize
-
+  def initialize(g_name, wallet, fav_song)
+    @g_name = g_name
+    @wallet = wallet
+    @fav_song = fav_song
   end
 
 end
--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -5,7 +5,11 @@ require_relative ("../guest")
 class TestGuest < Minitest::Test
 
   def setup
-
+    @guest1 = Guest.new("Jake",20,"song1")
   end
 
+  def test_guest_has_name
+    assert_equal("Jake", @guest1.g_name)
+  end
+  
 end
