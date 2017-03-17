8d3d0b5fd65c9708f700782b5396b5bd3605cf3a
Dominic <mrdjfraser@gmail.com>
2017-03-17 14:20:39 +0000

    test_room_has_spaces passing

--- room.rb
+++ room.rb
@@ -1,10 +1,15 @@
 class Room
 
-  attr_reader
+  attr_reader :room_name, :spaces
 
-  def initialize()
-
+  def initialize(room_name,spaces)
+    @room_name = room_name
+    @spaces = spaces
+    @playlist = []
+    @current_guests = []
   end
+
+
 
 
 
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,5 +1,6 @@
 require ("minitest/autorun")
 require ("minitest/rg")
+#require ('minitest/emoji')
 require_relative ("../guest")
 require_relative ("../song")
 require_relative ("../party")
@@ -16,11 +17,13 @@ class TestParty < Minitest::Test
     @song2 = Song.new("song2",2)
     @party1 = Party.new()
     @party2 = Party.new(@guest1)
-
+    @room1 = Room.new("first_room", 3)
   end
 
 
-
+def test_room_has_spaces
+  assert_equal(3, @room1.spaces)
+end
 
 
 
