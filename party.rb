e344ff34a81028df52bdb926803e4c147c1b05de
Dominic <mrdjfraser@gmail.com>
2017-03-17 12:45:55 +0000

    test_add_guest_to_party passing

--- party.rb
+++ party.rb
@@ -1,9 +1,14 @@
 class Party 
 
-  attr_reader
+  attr_reader :guests, :lowest_wallet
 
   def initialize()
+    @guests = []
+    @lowest_wallet = 0
+  end
 
+  def add_guest(guest)
+    @guests << guest
   end
 
 end
--- specs/party_spec.rb
+++ specs/party_spec.rb
@@ -2,15 +2,23 @@ require ("minitest/autorun")
 require ("minitest/rg")
 require_relative ("../guest")
 require_relative ("../song")
+require_relative ("../party")
+
 
 
 class TestParty < Minitest::Test
 
   def setup
-
+    @guest1 = Guest.new("Jake",20,"song1")
+    @guest2 = Guest.new("Sam",20,"song2")
+    @song1 = Song.new("song1",2.5)
+    @song2 = Song.new("song2",2)
+    @party1 = Party.new()
   end
 
-
+  def test_add_guest_to_party
+    assert_equal([@guest1], @party1.add_guest(@guest1))
+  end
 
 
 end
